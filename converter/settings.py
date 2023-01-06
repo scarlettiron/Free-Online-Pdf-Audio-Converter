54969a4d2b6aa31aba25453630d6d2adfd792bf9
scarlettiron <scottscarlett@gmail.com>
2023-01-06 01:07:52 -0500

    over rided django's testing to enable application to run without a database connected

--- converter/settings.py
+++ converter/settings.py
@@ -64,11 +64,9 @@ WSGI_APPLICATION = 'converter.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
+TEST_RUNNER = 'testing.DatabaselessTestRunner'
 
 
 # Password validation
